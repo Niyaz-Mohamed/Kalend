b8e268bf51f5409879ebdfc515bdfc694151cfa2
MshNiyaz <niyazshahul2006@gmail.com>
2021-08-09 12:44:23 +0800

    Created login and signup form models and validation

---
+++ lib/forms.py
@@ -0,0 +1,74 @@
+from typing import Pattern
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, ValidationError
+
+import re
+import requests
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+    def validate_username(form,field):
+        username=field.data
+
+        if not username.isalnum:
+            modifiedUsername=username.replace('_','a')
+            if not modifiedUsername.isalnum:
+                raise ValidationError("Username can only contain letters, numbers, or underscores")
+
+        if len(username) < 8:
+            raise ValidationError("Username must have >8 characters")
+        elif len(username) > 20:
+            raise ValidationError("Username must have <20 characters")
+
+    def validate_password(form,field):
+        password=field.data
+        if len(password) < 8:
+            raise ValidationError("Password must have >8 characters")
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    email= EmailField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+    def validate_username(form,field):
+        username=field.data
+
+        if not username.data.isalnum:
+            modifiedUsername=username.replace('_','a')
+            if not modifiedUsername.isalnum:
+                raise ValidationError("Username can only contain letters, numbers, or underscores")
+
+        if len(username) < 8:
+            raise ValidationError("Username must have >8 characters")
+        elif len(username) > 20:
+            raise ValidationError("Username must have <20 characters")
+
+    def validate_password(form,field):
+        password=field.data
+        if len(password) < 8:
+            raise ValidationError("Password must have >8 characters")
+
+    def validate_email(form,field):
+        email=field.data
+        email_regex = re.compile(r"[^@]+@[^@]+\.[^@]+")
+
+        response = requests.get(
+        "https://isitarealemail.com/api/email/validate",
+        params = {'email': email})
+        status = response.json()['status']
+
+        if email_regex.match(email):
+            pass
+        else:
+            raise ValidationError('Invalid Email')
+        
+        if status == "valid":
+            pass
+        elif status == "invalid":
+            ValidationError('Email does not exist')
+        else:
+            ValidationError('Email unknown')
--- lib/views.py
+++ lib/views.py
@@ -1,14 +1,38 @@
-from lib import app
+from os import error
 from flask import render_template, request, url_for, redirect
+from flask.helpers import flash
+
+from lib import app
+from lib.forms import LoginForm, SignUpForm
 
 @app.route('/')
 def index():
   return render_template('static.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-  return render_template('login.html')
+  form = LoginForm()
 
-@app.route('/signup')
+  if request.method == 'POST':
+    if form.validate_on_submit():
+      #login
+      pass
+    else:
+      #Handle errors
+      flash('Failure to submit form {}'.format(form.errors))
+ 
+  return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET','POST'])
 def signup():
-  return render_template('signup.html')
+  form = SignUpForm()
+
+  if request.method == 'POST':
+    if form.validate_on_submit():
+      #signup
+      pass
+    else:
+      #Handle errors
+      flash('Failure to submit form {}'.format(form.errors))
+  
+  return render_template('signup.html',form=form)
